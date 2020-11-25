338a6bb2638f3f39dbd6436bc0c462b352bd7417
Patrick Graf <grafpatron@github.com>
2020-11-25 19:48:32 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const fs = require("fs");
 const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION"]});
-// partials -> things that will load even after starting the bot
+//partials -> things that will load even after starting the bot
 const { registerCommands, registerEvents } = require('./utils/registry');
 
 (async () => {
