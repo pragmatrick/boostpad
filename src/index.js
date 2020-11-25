f55b021c0decb5d58b473a931296ef1178708cf5
Patrick Graf <grafpatron@github.com>
2020-11-25 20:35:20 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const { registerCommands, registerEvents } = require('./utils/registry');
 (async () => {
     client.commands = new Map();
     client.cachedMessageReactions = new Map();
-    client.config = JSON.parse(fs.readFileSync("config.json", "utf-8"));
+    client.config = JSON.parse(fs.readFileSync("./config.json", "utf-8"));
     await registerEvents(client, '../events');
     await registerCommands(client, '../commands');
     client.login(process.env.token);
