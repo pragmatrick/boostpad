f7f0779273a834cb1b66de48ab9ca53d4ee3b77e
Patrick Graf <grafpatron@github.com>
2020-11-27 18:56:58 +0100

    hack command update

--- src/commands/cb/hack.js
+++ src/commands/cb/hack.js
@@ -19,7 +19,7 @@ module.exports = {
             if (tag.id === msg.author.id) {
                 return msg.channel.send(`${msg.member} I can't hack you, tell me someone else to hack`);
             }
-            if (tag.id === client.config.users.grafpatron || tag.id === client.config.users.boostpad) {
+            if (client.config.users.includes(tag.id)) {
                 await msg.react("🇸");
                 await msg.react("🇮");
                 await msg.react("🇰");
