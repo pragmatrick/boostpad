1b2ef4a79b64607f8e481aade417504386265812
Patrick Graf <grafpatron@github.com>
2020-11-29 16:51:52 +0100

    Update roles.js

--- src/commands/embeds/roles.js
+++ src/commands/embeds/roles.js
@@ -58,7 +58,8 @@ module.exports = {
 | enjoy getting ignored and disrespected
 | nobody will help you anymore :stuck_out_tongue_closed_eyes:\n
 | reserved for <@${client.config.users.berkay}>\n\u200B`, inline: true},
-            {name: "__Gaming Roles__\n\u200B", value: 
+            {name: "__Gaming Roles__\n\u200B", value: "_group you into the associated communities._"},
+            {name: `\u200B`, value:  
 `<@&${client.config.roles.cod}>
 <@&${client.config.roles.impostor}>
 <@&${client.config.roles.trader}>
@@ -72,7 +73,7 @@ module.exports = {
 | <:${msg.guild.emojis.cache.get(client.config.emojis.r6).identifier}>
 | <:${msg.guild.emojis.cache.get(client.config.emojis.impostor).identifier}>
 | :crown:`, inline: true},
-            {name: "__Just For Fun__", value: justForFun});
+            {name: "__Just For Fun__\n\u200B", value: justForFun});
         await msg.channel.send(embed);
         msg.delete();
     }
