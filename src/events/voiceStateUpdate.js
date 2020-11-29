c464217cfa9b8c20f1e48f5df3083fb51daff601
Patrick Graf <grafpatron@github.com>
2020-11-29 12:50:06 +0100

    Update voiceStateUpdate.js

--- src/events/voiceStateUpdate.js
+++ src/events/voiceStateUpdate.js
@@ -1,10 +1,8 @@
 module.exports = (client, oldMember, newMember) => {
-    if (oldMember.channel && oldMember.channel.deleted) return;
-
     // Delete clear channels
     client.guilds.cache.get(client.config.server_id).channels.cache.array().forEach(channel => {
         if ((channel.parent == client.config.create_channels.vc.parent && channel.id != client.config.create_channels.vc.id)) {
-            if (!channel.deleted && channel.members.size == 0) channel.delete();
+            if (!channel.deleted && channel.members.size == 0) channel.delete().catch(err => console.log(err));
         }
     });
     // Join to create VC
