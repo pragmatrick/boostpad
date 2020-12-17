054df4c1b073b74ae9ea064112369b4fc9cd6e95
Patrick Graf <grafpatron@github.com>
2020-12-17 18:35:26 +0100

    Update gif.js

--- src/commands/everyone/gif.js
+++ src/commands/everyone/gif.js
@@ -5,11 +5,14 @@ module.exports = {
     description: "Nudges a user.",
     async execute(client, msg, args) { 
         if (msg.channel.id === client.config.channels.memes) {
-            const url = `https://api.tenor.com/v1/search?q=excited&key=${process.env.tenor}&&limit=8`;
+            let keywords = "rickroll";
+            if (args.length > 0) keywords = args.join(" ");
+            const url = `https://api.tenor.com/v1/search?q=${keywords}&key=${process.env.tenor}&limit=8&contentfilter=high`;
             const fetchedURL = await fetch(url);
             const file = await fetchedURL.json();
             const randomIndex = Math.random() * file.results.length;
-            msg.channel.send(file.results[randomIndex].url);
+            await msg.channel.send(file.results[randomIndex].url);
+            await msg.delete();
         } else {
             msg.react("🤨");
         }
