05ce86cf760e50c2c5e9c51a0d039f2174e83d64
Patrick Graf <grafpatron@github.com>
2020-12-17 18:38:13 +0100

    Update gif.js

--- src/commands/everyone/gif.js
+++ src/commands/everyone/gif.js
@@ -10,7 +10,7 @@ module.exports = {
             const url = `https://api.tenor.com/v1/search?q=${keywords}&key=${process.env.tenor}&limit=8&contentfilter=high`;
             const fetchedURL = await fetch(url);
             const file = await fetchedURL.json();
-            const randomIndex = Math.random() * file.results.length;
+            const randomIndex = Math.floor(Math.random() * file.results.length)
             await msg.channel.send(file.results[randomIndex].url);
             await msg.delete();
         } else {
