a10d6e6f7e3837f96e8e06fdee47a4dbbff0915f
grafpatron <grafpatron@gmail.com>
2021-03-20 12:01:51 +0100

    Update coinflip.js

--- src/commands/everyone/coinflip.js
+++ src/commands/everyone/coinflip.js
@@ -3,12 +3,9 @@ module.exports = {
     permissions: [],
     description: "Sends a random emoji, either BigBoost-Coin, or a SmallBoost-Coin.",
     execute(client, msg, args) { 
-        let random = Math.floor(Math.random() * 2);
-        if (random == 0) {
-            msg.channel.send(`<:${msg.guild.emojis.cache.get(client.config.emojis.coinflip_big).identifier}>`);
-        }
-        else {
-            msg.channel.send(`<:${msg.guild.emojis.cache.get(client.config.emojis.coinflip_small).identifier}>`);
-        }
+        let answer = Math.random() < 0.5 
+        ? `<:${msg.guild.emojis.cache.get(client.config.emojis.coinflip_big).identifier}>`
+        : `<:${msg.guild.emojis.cache.get(client.config.emojis.coinflip_small).identifier}>`;
+        msg.channel.send(answer);
     }
 }
