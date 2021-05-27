3a12f55db037b732c4ea17479f6a1fe00d314428
grafpatron <grafpatron@gmail.com>
2021-05-27 21:46:02 +0200

    Update guildMemberRemove.js

--- src/events/guildMemberRemove.js
+++ src/events/guildMemberRemove.js
@@ -2,6 +2,6 @@ module.exports = {
     name:        "guildMemberRemove",
     description: "Emitted whenever a member leaves a guild, or is kicked.",
     async run(client, member) {
-        member.guild.channels.cache.get(client.config.channels.greeting).send(`💨 ${member} has used up all his Boost.`);
+        member.guild.channels.cache.get(client.config.channels.greeting).send(`💨 ${member} has used up all the Boost.`);
     }
 }
